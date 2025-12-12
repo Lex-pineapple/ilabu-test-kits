7fa439b0bc7d1c4eb45974120c147991537171a7
Lex <pineapplepear55@gmail.com>
2025-12-12 13:44:59 +0300

    feat: add basic desktop layout and documentation

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,6 +1,14 @@
 import { Outlet } from "react-router-dom";
 
-import { Container, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  useMediaQuery,
+} from "@chakra-ui/react";
 
 import { ScrollToTop } from "#/components/scroll-to-top";
 import { CustomToaster } from "#/components/toaster";
@@ -11,28 +19,74 @@ import { modal } from "#shared/modal";
 
 import styles from "./layout.module.scss";
 
-export const Layout = () => (
-  <Flex
-    className={styles.root}
-    flexDir="column"
-    h="100%"
-    justifyContent="space-between"
-    m="0 auto"
-    maxW={500}
-    minH="100vh"
-    p={0}
-    pt={12}
-  >
-    <ScrollToTop />
+export const Layout = () => {
+  const [isLargerThan500] = useMediaQuery(["(min-width: 500px)"]);
+
+  return isLargerThan500 ? (
     <div>
       <Header />
-      <Container p={3.5} pb={0} pt={0}>
-        <Outlet />
-      </Container>
+      <Grid templateColumns="repeat(2, 1fr)">
+        <GridItem>
+          <Center>
+            <Flex
+              className={styles.root_mobile}
+              flexDir="column"
+              justifyContent="center"
+              margin="0 auto"
+              maxW={500}
+              minH="100vh"
+              p={0}
+              pt={12}
+            >
+              <ScrollToTop />
+              <div>
+                <Container p={3.5} pb={0} pt={0}>
+                  <Outlet />
+                </Container>
+              </div>
+            </Flex>
+          </Center>
+        </GridItem>
+        <GridItem>
+          <Box
+            bgAttachment="fixed"
+            bgImage="url('/bg-test.png')"
+            bgPos="center"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            h="100%"
+            w="100%"
+          />
+        </GridItem>
+      </Grid>
+      <Footer isDesktop={true} />
+      <CustomToaster />
+      <modal.Viewport />
+      <bottomSheetModal.Viewport />
     </div>
-    <Footer />
-    <CustomToaster />
-    <modal.Viewport />
-    <bottomSheetModal.Viewport />
-  </Flex>
-);
+  ) : (
+    <Flex
+      className={styles.root}
+      flexDir="column"
+      h="100%"
+      justifyContent="space-between"
+      m="0 auto"
+      maxW={500}
+      minH="100vh"
+      p={0}
+      pt={12}
+    >
+      <ScrollToTop />
+      <div>
+        <Header />
+        <Container p={3.5} pb={0} pt={0}>
+          <Outlet />
+        </Container>
+      </div>
+      <Footer isDesktop={false} />
+      <CustomToaster />
+      <modal.Viewport />
+      <bottomSheetModal.Viewport />
+    </Flex>
+  );
+};
