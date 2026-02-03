6f88e4c90ea298bd25db020bf5ea2e9228eb58b1
Lex <pineapplepear55@gmail.com>
2026-02-03 17:29:20 +0300

    feat(B-1): add tube data fetching to instruction

--- src/loaders/instruction-loader.ts
+++ src/loaders/instruction-loader.ts
@@ -2,6 +2,7 @@ import { redirect } from "react-router-dom";
 
 import { PATHS } from "#constants/paths";
 import { ordersApi } from "#store/api/orders-api";
+import { tubesApi } from "#store/api/tubes-api";
 import {
   setNotificationData,
   setNotificationVisibility,
@@ -10,10 +11,12 @@ import store from "#store/store";
 
 export async function loader() {
   const p = store.dispatch(ordersApi.endpoints.getOrderInstructions.initiate());
+  const p2 = store.dispatch(tubesApi.endpoints.getRequiredTubes.initiate());
 
   try {
     const response = await p.unwrap();
-    return response.instructions[0];
+    const response2 = await p2.unwrap();
+    return { instruction: response.instructions[0], tubes: response2.tubes };
   } catch {
     store.dispatch(setNotificationVisibility(true));
     store.dispatch(
